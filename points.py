130e47e96493211e6fdfc3395a79319694f1c6b9
adunlop1234 <dunlop.arthur@gmail.com>
2020-10-15 20:47:25 +0100

    Fixed bug in reformatting datatypes.

--- points.py
+++ points.py
@@ -11,9 +11,28 @@ defence = pd.read_csv('D_Week_1.csv')
 defence = defence.replace('-', 0)
 
 # Ensure each column correct datatype (default object)
-pd.to_numeric(defence, errors='ignore')
+defence.apply(pd.to_numeric, errors='ignore')
 
-#defence = defence.astype({'Sacks': 'int64'}).dtypes
+# Create dict of datatypes to reformat the column datatypes
+data_types = {
+	'Name': 'str',
+	'Position': 'str',
+	'Sacks': 'int64',
+	'Def INT': 'int64',
+	'Fum Rec': 'int64',
+	'Saf': 'int64',
+	'Def TD': 'int64',
+	'Def 2pt Ret': 'int64',
+	'Def Ret TD': 'int64',
+	'Pts Allowed': 'int64',
+	'Points Total': 'float64'
+}
+
+# By setting defence = defence.astype.dtypes it set the data frame to be a list of datatypes rather than update the datatype of each column.
+# The other thing to note is that the "object" datatype is a string of unlimited size. Otherwise you need to specify the max number of bytes in the string:
+# https://stackoverflow.com/questions/33957720/how-to-convert-column-with-dtype-as-object-to-string-in-pandas-dataframe
+print(defence.dtypes)
+defence = defence.astype(data_types)
 #defence["Sacks"].astype(str).astype(int).dtypes
 #defence["Saf"].astype(str).astype(int).dtypes
 print(defence.dtypes)
