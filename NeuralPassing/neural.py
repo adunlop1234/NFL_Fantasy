ea272494506d2df23df00baad8eee525b2fb2c86
BenjaminClayton <53317860+BenjaminClayton@users.noreply.github.com>
2021-01-22 08:53:57 +0000

    neural iteration 4

--- NeuralPassing/neural.py
+++ NeuralPassing/neural.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 import numpy as np
-import tensorflow as tf
+#import tensorflow as tf
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -10,20 +10,17 @@ import matplotlib.pyplot as plt
 
 # HYPERPARMETERS
 random_state = 1
-l2_regularisation = 500
-learning_rate = 0.001
-epochs = 100
+l1_regularisation = 1.5
+learning_rate = 0.0007
+epochs = 300
 batch_size = 10
 
 # Open raw features
 df = pd.read_csv(os.path.join('NeuralPassing','Data','qbPassingFeatures.csv'), index_col=0)
-column_types = ['pass_cmp', 'pass_att', 'pass_yds', 'pass_td', 'pass_int', 'pass_sacked', 'rush_att', 'rush_yds', 'rush_td', 'home_game', 'Label_pass_yds']
+column_types = ['pass_cmp', 'pass_att', 'pass_yds', 'pass_td', 'pass_int', 'pass_sacked', 'rush_att', 'rush_yds', 'rush_td', 'Label_pass_yds']
 # Get feature columns
 columns = [column for column in df.columns.values if len([i for i in column_types if i in column]) > 0]
 df = df[columns]
-
-# Convert Home, Away, BYE to one-hot
-df = pd.get_dummies(df)
 
 # Fill any Nans with 0 (very rare occurence, just Jalen Hurts for 2 games)
 df = df.fillna(0)
@@ -37,7 +34,7 @@ y = np.array(df.Label_pass_yds)
 
 # Split into training, validating and testing set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state)
-
+"""
 # Use a normalisation layer to normalise the features
 normalizer = tf.keras.layers.experimental.preprocessing.Normalization()
 normalizer.adapt(np.array(X_train))
@@ -48,9 +45,9 @@ model = tf.keras.models.Sequential([
     # Normalisation layer
     normalizer,
     # Add a first hidden layer
-    tf.keras.layers.Dense(40, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(l2_regularisation)),
+    tf.keras.layers.Dense(40, activation="relu", kernel_regularizer=tf.keras.regularizers.l1(l1_regularisation)),
     # Add a second hidden layer
-    tf.keras.layers.Dense(10, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(l2_regularisation)),
+    tf.keras.layers.Dense(10, activation="relu", kernel_regularizer=tf.keras.regularizers.l1(l1_regularisation)),
     # Add an output layer
     tf.keras.layers.Dense(1)
 
@@ -59,7 +56,7 @@ model = tf.keras.models.Sequential([
 # Compile neural network
 model.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
-    loss="mean_squared_error"
+    loss="mean_absolute_error"
 )
 
 # Fit model on training data
@@ -70,6 +67,7 @@ def plot_loss(history):
     plt.plot(history.history['val_loss'], label='val_loss')
     plt.xlabel('Epoch')
     plt.ylabel('Error')
+    plt.ylim([0,150])
     plt.legend()
     plt.show()
 
@@ -86,3 +84,4 @@ plt.ylabel('Predict')
 plt.xlim([0,500])
 plt.ylim([0,500])
 plt.show()
+"""
