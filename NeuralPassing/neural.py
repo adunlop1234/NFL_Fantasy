827613c14d97956c8c2a9a4384b93cd1397e1ce3
BenjaminClayton <53317860+BenjaminClayton@users.noreply.github.com>
2021-01-24 09:40:41 +0000

    xgboost bug fix

--- NeuralPassing/neural.py
+++ NeuralPassing/neural.py
@@ -42,7 +42,7 @@ y = np.array(df.Label_pass_yds)
 
 # Split into training, validating and testing set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state)
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.25, random_state=random_state)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=random_state)
 
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_absolute_error
