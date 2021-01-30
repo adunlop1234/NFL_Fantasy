45f84c1b5c85529776438ebce4d7c06827f71ab3
BenjaminClayton <53317860+BenjaminClayton@users.noreply.github.com>
2021-01-30 09:27:24 +0000

    replaced missing values with mean (instead of 0)

--- NeuralPassing/features.py
+++ NeuralPassing/features.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import statistics
 import numpy as np
+from numpy import nan
 
 ### Create dictionaries to store data
 
@@ -13,8 +14,8 @@ ref = pd.read_csv(os.path.join('References','teams.csv'))
 nfl_teams = list(pd.read_csv(os.path.join('References','teams.csv')).Abrev)
 
 # Create dictionaries to store data (by default a BYE week)
-features_D = {'home_game': 'BYE', 'pass_cmp' : 0, 'pass_att' : 0, 'pass_yds' : 0, 'pass_td' : 0, 'pass_int' : 0, 'pass_sacked' : 0, 'rush_att' : 0, 'rush_yds' : 0, 'rush_td' : 0}
-features_O = {'rush_att' : 0, 'opp' : 0, 'rush_yds' : 0, 'rush_td' : 0}    
+features_D = {'home_game': 'BYE', 'pass_cmp' : nan, 'pass_att' : nan, 'pass_yds' : nan, 'pass_td' : nan, 'pass_int' : nan, 'pass_sacked' : nan, 'rush_att' : nan, 'rush_yds' : nan, 'rush_td' : nan}
+features_O = {'rush_att' : nan, 'opp' : 0, 'rush_yds' : nan, 'rush_td' : nan}    
     
 # Loop over each season
 for season in [15, 16, 17, 18, 19, 20]:
@@ -36,7 +37,7 @@ for season in [15, 16, 17, 18, 19, 20]:
 player_data = {}
 
 # Create dictionary to store data (by default player is NO_PLAY (either BYE or OUT injured))
-features_P = {'team' : 0, 'home_game': 'NO_PLAY', 'pass_cmp' : 0, 'pass_att' : 0, 'pass_yds' : 0, 'pass_td' : 0, 'pass_int' : 0, 'pass_sacked' : 0, 'rush_att' : 0, 'rush_yds' : 0, 'rush_td' : 0}
+features_P = {'team' : 0, 'home_game': 'NO_PLAY', 'pass_cmp' : nan, 'pass_att' : nan, 'pass_yds' : nan, 'pass_td' : nan, 'pass_int' : nan, 'pass_sacked' : nan, 'rush_att' : nan, 'rush_yds' : nan, 'rush_td' : nan}
 
 # Loop over each season
 for season in [15, 16, 17, 18, 19, 20]:
@@ -136,7 +137,7 @@ for season, season_dict in player_data.items():
         for week in range(7,17):
 
             # Check if QB didn't play week
-            if player_data[season][qb][week]['pass_yds'] == 0:
+            if np.isnan(player_data[season][qb][week]['pass_yds']):
                 continue
 
             # Add data for ease of checking
@@ -147,8 +148,12 @@ for season, season_dict in player_data.items():
             # Add label
             features.loc[c, ('Label','pass_yds')] = player_data[season][qb][week]['pass_yds']
 
-            team = player_data[season][qb][week]['team']
-
+            i = 1
+            while i < 18:
+                if player_data[season][qb][i]['team'] != 0:
+                    team = player_data[season][qb][i]['team']
+                    break
+                i += 1
 
             # Loop over preceeding weeks
             for preceeding in range(1,7):
@@ -181,7 +186,6 @@ for season, season_dict in player_data.items():
 
 
             # Populate upcoming defence mean features
-            # Populate previous games defence features
             opp = team_data[season][team][week]['O']['opp']
             f_D = {f : [] for f in features_D.keys() if f != 'home_game'}
             # Calculate mean value for 6 weeks previous to upcoming week
@@ -194,14 +198,12 @@ for season, season_dict in player_data.items():
             for feature in [f for f in features_D.keys() if f != 'home_game']:
                 features.loc[c, ('Defence_upcom_mean', feature + '_upcoming')] = statistics.mean(f_D[feature])
 
-            # Check for null values
-            if features.loc[c].isnull().values.any():
-                null_count += 1
-                features.loc[c] = np.nan
-            else:
-                c += 1
+            c += 1
 
-print(str(null_count) + ' rows were removed due to containing NULL values')
+# Fill missing values (due to BYE weeks) with mean values
+for column in features.columns:
+    features.loc[:,column] = features.loc[:,column].astype(float, errors='ignore')
+features = features.fillna(features.mean())
 
 # Save features as DataFrame
 features.to_csv(os.path.join('NeuralPassing','Data','qbPassingFeatures.csv'))
